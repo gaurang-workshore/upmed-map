e3f43f6618a990a87d17090d09810cd158fb23eb
gaurang-workshore <122290554+gaurang-workshore@users.noreply.github.com>
2023-09-13 21:09:42 +0530

    Update map-configuration.js

--- map-configuration.js
+++ map-configuration.js
@@ -1,55 +1,30 @@
-function createmap(e) {
-  let n = document.getElementsByClassName(e.title),
-      t = document.getElementsByClassName(e.latitude),
-      s = document.getElementsByClassName(e.longitude),
-      gmap = document.getElementsByClassName(e.googlemap),
-      amap = document.getElementsByClassName(e.applemap),
-      o = document.getElementsByClassName(e.listingdescription),
-      l = [],
-      a = [];
+// Import GSAP and ScrollTrigger if you haven't already
 
-  for (M = 0; M < n.length; M++) {
-    if ("" != t[M].innerHTML && "" != s[M].innerHTML) {
-      a.push({
-        tittle: n[M].innerHTML,
-        lat: Number(t[M].innerHTML),
-        lng: Number(s[M].innerHTML),
-        contdiv: '<div class="info-box"><p class="projname">' + n[M].innerHTML + '</p><p class="projdesc">' + o[M].innerHTML + '</p><a class="um-texts" href="' + gmap[M].innerHTML + '" rel="noopener" target="_blank">Open in Google Map</a><br><a class="um-texts" href="' + amap[M].innerHTML + '" rel="noopener" target="_blank">Open in Apple Map</a></div>',
-      });
-    }
-  }
+// Set up ScrollTrigger
+gsap.registerPlugin(ScrollTrigger);
 
-  const g = new google.maps.Map(document.getElementById("map-div"), {
-    zoom: 9,
-    center: a[0],
-    disableDefaultUI: true
-  });
+// Create a timeline for the animation
+const animationTimeline = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".section_intro",
+    start: "top 30%", // Adjust the start value to control when the animation starts
+    end: "top 70%",   // Adjust the end value to control when the animation ends
+    scrub: 0.5,       // Adjust the scrub value to make the animation slower and smoother
+  },
+});
 
-  const p = new google.maps.InfoWindow();
+animationTimeline.to(".section_intro", {
+  padding: "0",
+  ease: "none", // Change the ease mode to "none" for linear animation
+});
 
-  for (M = 0; M < a.length; M++) {
-    let icon = {
-      url: 'https://uploads-ssl.webflow.com/631645b2553f411a30172973/645b81b13eff5d82cd826973_map%20pin.svg',
-      scaledSize: new google.maps.Size(50, 50),
-      anchor: new google.maps.Point(25, 50)
-    };
+animationTimeline.to(".cta41_component", {
+  borderRadius: "0",
+  ease: "none", // Change the ease mode to "none" for linear animation
+}, 0);
 
-    let marker = new google.maps.Marker({
-      position: a[M],
-      map: g,
-      title: a[M].tittle,
-      icon: icon,
-    });
-
-    l.push(marker);
-
-    v(marker, a[M].contdiv);
-  }
-
-  function v(marker, content) {
-    marker.addListener("click", () => {
-      p.setContent(content);
-      p.open(g, marker);
-    });
-  }
-}
+animationTimeline.to(".intro", {
+  maxWidth: "100vw",
+  width: "100%", // Corrected property name
+  ease: "none", // Change the ease mode to "none" for linear animation
+}, 0);
